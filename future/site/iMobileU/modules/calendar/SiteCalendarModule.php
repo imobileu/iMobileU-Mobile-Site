2af439399074d673f5049dc181af3d90d87e2886
Pete Akins <pete.akins@modolabs.com>
2010-12-23 09:50:48 -0500

    fixed regression in calendar detail

--- future/site/iMobileU/modules/calendar/SiteCalendarModule.php
+++ future/site/iMobileU/modules/calendar/SiteCalendarModule.php
@@ -5,6 +5,7 @@ class SiteCalendarModule extends CalendarModule {
   protected function initializeForPage() {
     switch ($this->page) {
       case 'list':
+      case 'detail':
         parent::initializeForPage();
         break;
        default:
